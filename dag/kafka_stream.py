12cc03cf1c7d711f956343b26a3fafceaa21c2c2
Beryl Atieno <berylatieno30@gmail.com>
2023-12-11 12:41:23 +0300

    successfully stream data in the created format in stream_data func

--- dag/kafka_stream.py
+++ dag/kafka_stream.py
@@ -8,17 +8,51 @@ default_arguments = {
     "start_date": datetime(2023, 12, 3)
 }
 
-def stream_data():
-    import json
+#get data from random user generator API
+
+def get_data():
     import requests
 
     response = requests.get('https://randomuser.me/api/')
     if response.status_code == 200:
-        data = response.json()
-        data = data['results'][0]
-        print(json.dumps(data, indent=3))
+        response = response.json()
+        response = response['results'][0]
+
+        return response
     else:
         print(f"Error: {response.status_code} - {response.text}")
+
+#Formating the API response into a dictionary
+
+def format_data(response):
+    data = {}
+    data['first_name'] = response['name']['first']
+    data['last_name'] = response['name']['last']
+    data['gender'] = response['gender']
+    data['address'] = (str(response['location']['street']['number']) + " " 
+                       + str(response['location']['street']['name']) + " " 
+                       + str(response['location']['city']) + " " 
+                       + str(response['location']['state']) + " " 
+                       + str(response['location']['country']))
+    data['post_code'] = response['location']['postcode']
+    data['email'] = response['email']
+    data['username'] = response['login']['username']
+    data['age'] = response['dob']['age']
+    data['dob'] = response['dob']['date']
+    data['registration_date'] = response['registered']['date']
+    data['phone'] = response['phone']
+    data['picture'] = response['picture']['medium']
+
+    return data
+
+
+#Streaming the data
+def stream_data():
+    import json
+    res = get_data()
+    res = format_data(res)
+    print(json.dumps(res, indent=3))
+    
 
 with DAG("user_automation",
          default_args=default_arguments,
@@ -31,5 +65,4 @@ with DAG("user_automation",
         task_id = "stream_data_from_api",
         python_callable=stream_data
     )
-
 stream_data()
